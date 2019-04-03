2778b4776f33954f8308bebba44c5f4b5087599b
bmcmahen <ben.mcmahen@gmail.com>
2019-04-03 15:32:07 -0700

    add working toast example

---
+++ src/components/ToastExample.tsx
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import * as React from "react";
+import useToast from "sancho/build/Toast";
+import { Button } from "sancho";
+
+export interface ToastExampleProps {}
+
+export const ToastExample: React.FunctionComponent<
+  ToastExampleProps
+> = props => {
+  const toast = useToast();
+
+  function play() {
+    ["top-left", "bottom-left"].map(position => {
+      toast({
+        title: "Hello world!",
+        duration: 3000,
+        position: position as any
+      });
+    });
+
+    ["info", "success", "question", "danger", "warning"].map((intent, i) => {
+      setTimeout(() => {
+        toast({
+          title: intent,
+          intent: intent as any,
+          position: "top-left"
+        });
+      }, (i + 1) * 500);
+    });
+  }
+
+  return <Button onClick={play}>Trigger toast example</Button>;
+};
